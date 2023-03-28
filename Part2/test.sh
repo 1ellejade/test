889e8a0addd75f32783dc4e863732f6faeddb110
1ellejade <107277977+1ellejade@users.noreply.github.com>
2023-03-28 11:37:09 -0400

    Update test.sh

--- Part2/test.sh
+++ Part2/test.sh
@@ -3,7 +3,7 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /Part2/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /Part2/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /Part2/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../shot_logs.csv /Part2/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../shot_logs.csv /Part2/input/
 
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
 -file ../Part2/mapper1.py -mapper ../Part2/mapper1.py \
